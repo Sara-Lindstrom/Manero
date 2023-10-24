f567d9ddbfacf87221f2a580bf1f6a4dbe549c0a
lirije <lirije11@hotmail.com>
2023-10-24 14:28:40 +0200

    three unit tests working one integrationtest in progress

---
+++ WebApi.Tests/IntegrationTests/UserController_Tests.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using System.Text;
+using WebApi;
+
+namespace WebApi.Tests.IntegrationTests;
+
+[Collection("IntegrationTestCollection")]
+public class UserController_Tests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public UserController_Tests(WebApplicationFactory<Program> factory)
+	{
+        _factory = factory;
+        _client = factory.CreateClient();
+
+
+        //_client = new HttpClient
+        //{
+        //    BaseAddress = new Uri("http://localhost:3000"), // Replace with the URL of your running application.
+        //};
+    }
+
+    [Fact]
+    public async Task CreateUser_Should_Return_Conflict_When_User_Exists()
+    {
+        // Arrange: Prepare a user model to create
+        var userModel = new
+        {
+            Email = "test@example.com",
+            Password = "Password123"
+        };
+
+        // Act: Send a POST request to the CreateUser endpoint
+
+        var requestContent = new StringContent(
+               System.Text.Json.JsonSerializer.Serialize(userModel),
+               Encoding.UTF8,
+               "application/json");
+
+        var response = await _client.PostAsync("/api/User/CreateUser", requestContent);
+
+        // Assert: Check the HTTP response status code
+        Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+    }
+}
---
+++ WebApi.Tests/UnitTests/UserController_Tests.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Moq;
+using WebApi.Context;
+using WebApi.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Tests.UnitTests;
+
+public class UserController_Tests
+{
+    private readonly Mock<UserManager<UserModel>> _mockUserManager;
+    private readonly Mock<UserDbContext> _mockUserDbContext;
+    private readonly Mock<SignInManager<UserModel>> _mockSignInManager;
+    private readonly UserController _userController;
+
+    public UserController_Tests()
+    {
+        //UserManager
+        var userStore = new Mock<IUserStore<UserModel>>(); 
+        _mockUserManager = new Mock<UserManager<UserModel>>(
+            userStore.Object,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+
+        //UserDbContext
+        _mockUserDbContext = new Mock<UserDbContext>(new DbContextOptions<UserDbContext>());
+
+        var users = new[]
+            {
+                new UserModel { Id = "1", UserName = "user1", Email = "user1@example.com" },
+                new UserModel { Id = "2", UserName = "user2", Email = "user2@example.com" }
+            }.AsQueryable();
+
+        var mockDbSet = new Mock<DbSet<UserModel>>();
+        mockDbSet.As<IQueryable<UserModel>>().Setup(m => m.Provider).Returns(users.Provider);
+        mockDbSet.As<IQueryable<UserModel>>().Setup(m => m.Expression).Returns(users.Expression);
+        mockDbSet.As<IQueryable<UserModel>>().Setup(m => m.ElementType).Returns(users.ElementType);
+        mockDbSet.As<IQueryable<UserModel>>().Setup(m => m.GetEnumerator()).Returns(() => users.GetEnumerator());
+
+        _mockUserDbContext.Setup(d => d.Users).Returns(mockDbSet.Object);
+
+        //SignInManager
+        var contextAccessor = new Mock<IHttpContextAccessor>();
+        var userPrincipalFactory = new Mock<IUserClaimsPrincipalFactory<UserModel>>();
+        var options = new Mock<IOptions<IdentityOptions>>();
+        var logger = new Mock<ILogger<SignInManager<UserModel>>>();
+        var schemes = new Mock<IAuthenticationSchemeProvider>();
+        var confirmation = new Mock<IUserConfirmation<UserModel>>();
+
+        _mockSignInManager = new Mock<SignInManager<UserModel>>(
+            _mockUserManager.Object, 
+            contextAccessor.Object, 
+            userPrincipalFactory.Object, 
+            options.Object,         
+            logger.Object,            
+            schemes.Object,           
+            confirmation.Object);
+
+        _userController = new UserController(_mockUserManager.Object, _mockUserDbContext.Object, _mockSignInManager.Object);
+    }
+
+    [Fact]
+    public async Task CreateAsync_ShouldReturnBadRequest_WhenMoodelStateIsInvalid()
+    {
+        //Arrange
+        var user = new User();
+        _userController.ModelState.AddModelError("Name", "Name is requried");
+
+        //Act
+        var result = await _userController.CreateUser(user);
+
+        //Assert
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    [Fact]
+    public async Task SignIn_ShouldReturnOk_WhenSignInSucceeds()
+    {
+        //Arrange
+        var user = new UserModel
+        {
+            UserName = "testuser",
+            Email = "testuser@example.com",
+        };
+
+        _mockSignInManager
+                .Setup(x => x.PasswordSignInAsync(user, "password", true, false))
+                .ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
+
+        var model = new User
+        {
+            Email = user.Email,
+            Password = "password",
+            RememberMe = true,
+        };
+
+        //Act
+        var result = await _userController.SignIn(model);
+
+        //Assert
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    [Fact]
+    public async Task SignIn_ShouldReturnBadRequest_WhenSignInFails()
+    {
+        // Arrange
+        var user = new UserModel
+        {
+            UserName = "testuser",
+            Email = "testuser@example.com",
+        };
+
+        _mockSignInManager
+            .Setup(x => x.PasswordSignInAsync(user, "invalid_password", true, false))
+            .ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Failed);
+
+        var model = new User
+        {
+            Email = user.Email,
+            Password = "invalid_password",
+            RememberMe = true,
+        };
+
+        // Act
+        var result = await _userController.SignIn(model);
+
+        // Assert
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+}
