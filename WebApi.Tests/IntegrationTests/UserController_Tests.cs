db1a34ad0a2b2fd59f84179f8a8cf46dc6bed0f9
lirije <lirije11@hotmail.com>
2023-10-25 08:30:08 +0200

    testing new way of integrationstesting

--- WebApi.Tests/IntegrationTests/UserController_Tests.cs
+++ WebApi.Tests/IntegrationTests/UserController_Tests.cs
@@ -1,32 +1,42 @@
 ﻿
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using System.Net;
 using System.Text;
 using WebApi;
+using WebApi.Context;
+using WebApi.Models;
 
 namespace WebApi.Tests.IntegrationTests;
 
 public class UserController_Tests 
 {
-    private readonly HttpClient _client;
+    private readonly UserDbContext _context;
+    private readonly UserController _controller;
 
-    public UserController_Tests(WebApplicationFactory<Program> factory)
+    public UserController_Tests()
 	{
-        _client = factory.CreateClient();
+        var DBoptions = new DbContextOptionsBuilder<UserDbContext>()
+            .UseInMemoryDatabase(databaseName: "test")
+            .Options;
 
-        //_client = new HttpClient
-        //{
-        //    BaseAddress = new Uri("http://localhost:3000"), // Replace with the URL of your running application.
-        //};
+        _context = new UserDbContext(DBoptions);
+
+        var userManager = new UserManager<UserModel>(new UserStore<UserModel>(_context), null, null, null, null, null, null, null, null);
+        var signInManager = new SignInManager<UserModel>(userManager, new HttpContextAccessor(), new Mock<IUserClaimsPrincipalFactory<UserModel>>().Object, null, null, null, null);
+
+        _controller = new UserController(userManager, _context, signInManager);
     }
 
     [Fact]
     public async Task CreateUser_Should_Return_Conflict_When_User_Exists()
     {
         // Arrange: Prepare a user model to create
-        var userModel = new
+        var userModel = new User
         {
             Email = "test@example.com",
             Password = "Password123"
@@ -34,14 +44,10 @@ public class UserController_Tests
 
         // Act: Send a POST request to the CreateUser endpoint
 
-        var requestContent = new StringContent(
-               System.Text.Json.JsonSerializer.Serialize(userModel),
-               Encoding.UTF8,
-               "application/json");
-
-        var response = await _client.PostAsync("/api/User/CreateUser", requestContent);
+        var result = await _controller.CreateUser(userModel);
 
         // Assert: Check the HTTP response status code
-        Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        var conflictResult = Assert.IsType<ConflictResult>(result);
+        Assert.Equal((int)HttpStatusCode.Conflict, conflictResult.StatusCode);
     }
 }
