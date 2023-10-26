321ee891c662b1e0da06c586b867f78c18bb968d
Simon <siiimonngummesson@gmail.com>
2023-10-26 11:23:39 +0200

    fixed test again

--- WebApp/src/__tests__/addCardOnChange.Test.tsx
+++ WebApp/src/__tests__/addCardOnChange.Test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import AddCardSection from '../sections/AddCardSection';
 
 test('input field onChange works', () => {
