994aeed421f1de4a3edb243e7cc8eb11e8c60a0c
LinusFahlander <linusfahlander43@gmail.com>
2023-10-30 17:50:17 +0100

    I added sub header for bestseller on home page

---
+++ WebApp/src/sections/BestSellerPreviewSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BestSellerPreviewSection = () => {
+
+  return (
+    <section className='best-seller-preview'>
+        <div className='container best-seller-subheader'>
+            <h2>Best Seller</h2>
+            <a href="#">view all<i className="fa-solid fa-chevron-right"></i></a>
+        </div>
+    </section>
+  )
+}
+
+export default BestSellerPreviewSection
--- WebApp/src/views/HomeView.tsx
+++ WebApp/src/views/HomeView.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import BestSellerPreviewSection from '../sections/BestSellerPreviewSection'
 
 const HomeView = () => {
   return (
+
+    <>
     <section className='home-page-showcase'>
-        <div className='container showcase-offers'>
-            <h2>Take 50% off!</h2>
-            <a href="#" className='btn white-btn'>SHOP NOW</a>
-        </div>
+      <div className='container showcase-offers'>
+        <h2>Take 50% off!</h2>
+        <a href="#" className='btn white-btn'>SHOP NOW</a>
+      </div>
     </section>
+    
+    <BestSellerPreviewSection />
+    </>
+    
   )
 }
 
