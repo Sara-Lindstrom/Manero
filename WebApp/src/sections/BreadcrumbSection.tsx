6fc4a57baaa1afa6dd273b70e8137297b1387845
EIIinor <ellinor.ljus@utb.ecutbildning.se>
2023-10-30 11:33:17 +0100

    searchfield/logo/cart/hamburger icons added to breadcrumb

--- WebApp/src/sections/BreadcrumbSection.tsx
+++ WebApp/src/sections/BreadcrumbSection.tsx
@@ -1,26 +1,69 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import logo from '../Images/logo.svg'
+import SearchField from '../components/SearchField';
 
 interface IBreadcrumbProps {
-  currentPage: string
-  showBackButton?: boolean; // visa eller dölj knappen funktion
-  onNavigateBack?: () => void; // funktion som anropas när användaren klickar på knappen för att  gå tillbaka
+ 
+  showBackButton?: boolean; // show backbutton
+  onNavigateBack?: () => void; //method for going back
+  showHamburgerButton?: boolean; // show hamburgerbutton
+
+  currentPage: string // current page string 
+  showCurrentPage?: boolean; // show current page
+  showSearchField?: boolean; // show search field
+
+  showCartItem?: boolean; // show cart
 }
 
-const BreadcrumbSection: React.FC<IBreadcrumbProps> = ( {currentPage, showBackButton, onNavigateBack } ) => {
+const BreadcrumbSection: React.FC<IBreadcrumbProps> = ( {currentPage, showBackButton, onNavigateBack, showCartItem, showCurrentPage, showSearchField, showHamburgerButton } ) => {
   return (
     <section className='breadcrumb'>
         <div className='container'>
             <ul className='breadcrumb-list'>
 
-              <li className='backButton'>
-                {showBackButton && onNavigateBack && (
-                  <button onClick={onNavigateBack}>
-                    <i className="fa-solid fa-chevron-left"></i>
-                  </button>
-                )}
-              </li>
+            <div className='first-column'>
+              {showHamburgerButton ? ( // Show hamburger if showHamburgerButton is true
+                <button className='hamburgerButton'>
+                  <i className="fa-regular fa-bars-staggered"></i>
+                </button>
+              ) : ( // Show backButton if showHamburgerButton is false or showBackButton is true
+                <li className='backButton'>
+                  {showBackButton && onNavigateBack && (
+                    <button onClick={onNavigateBack}>
+                      <i className="fa-solid fa-chevron-left"></i>
+                    </button>
+                  )}
+                </li>
+              )}
+            </div>
 
-              <li className='currentPage'>{currentPage}</li>
+
+              <div className='second-column'>
+                {/* If statement, if currentPage is true currentPage is showing, if seachField is true that one is showing ELSE logotype will show */}
+                  {showCurrentPage ? (
+                    <li className='currentPage'>{currentPage}</li>
+                  ) : showSearchField ? (
+                    <li className='searchField'>
+                      < SearchField />
+                    </li>
+                  ) : (
+                    <img className='logotype' src={logo} alt='logo' />
+                  )}
+              </div>
+
+
+              <div className='third-column'>
+                <li  className="cart">
+                  {showCartItem ? (
+                    <NavLink to={'/cart'}>
+                      <span className="translate-middle badge rounded-pill">1</span>
+                      <i className="fa-regular fa-bag-shopping"></i>
+                    </NavLink>              
+                  ) : null }             
+                </li>      
+              </div>
+                   
          
             </ul>
         </div>
