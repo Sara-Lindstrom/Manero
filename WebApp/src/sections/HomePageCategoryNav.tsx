0725a27268e0ac6e46f5fae6c85a31abc91c902c
LinusFahlander <linusfahlander43@gmail.com>
2023-11-01 17:30:21 +0100

    Fixed problem with css

--- WebApp/src/sections/HomePageCategoryNav.tsx
+++ WebApp/src/sections/HomePageCategoryNav.tsx
@@ -11,16 +11,16 @@ const HomePageCategoryNav = () => {
             <a href="/category#men" className='home-nav-text'>MEN</a>
           </div>
           <div className='home-page-nav-content'>
-          <img className='HomepageCategoryImage' src={homepageCategoryImage} alt='CategoryImage' />            
-          <a href="/category#women" className='home-nav-text'>WOMEN</a>
+            <img className='HomepageCategoryImage' src={homepageCategoryImage} alt='CategoryImage' />            
+            <a href="/category#women" className='home-nav-text'>WOMEN</a>
           </div>
           <div className='home-page-nav-content'>
-          <img className='HomepageCategoryImage' src={homepageCategoryImage} alt='CategoryImage' />            
-          <a href="/category#kids" className='home-nav-text'>KIDS</a>
+            <img className='HomepageCategoryImage' src={homepageCategoryImage} alt='CategoryImage' />            
+            <a href="/category#kids" className='home-nav-text'>KIDS</a>
           </div>
           <div className='home-page-nav-content'>
-          <img className='HomepageCategoryImage' src={homepageCategoryImage} alt='CategoryImage' />            
-          <a href="/category#accessories" className='home-nav-text'>ACCESSORIES</a>  
+            <img className='HomepageCategoryImage' src={homepageCategoryImage} alt='CategoryImage' />            
+            <a href="/category#accessories" className='home-nav-text'>ACCESSORIES</a>  
           </div>              
         </div>
       </div>
