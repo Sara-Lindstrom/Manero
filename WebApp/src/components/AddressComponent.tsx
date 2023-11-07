639bf81bb1f42b03c0a937cad2ccb7e0df509d40
lirije <lirije11@hotmail.com>
2023-11-07 15:34:45 +0100

    more design added

--- WebApp/src/components/AddressComponent.tsx
+++ WebApp/src/components/AddressComponent.tsx
@@ -18,17 +18,17 @@ const AddressComponent: React.FC<AddressComponentProps> = ({ address }) => {
             <div className='container'>
             <i className="address-icon fa-solid fa-house"></i>
             <div className='address-info'>
-                <p className='address-title'>Title: {address.Title}</p>
+                <p className='address-title'>{address.Title}</p>
                 <div className='address-small-info'>
-                    <p>Title: {address.Title}</p>
-                    <p>Street: {address.StreetName}</p>
-                    <p>City: {address.City}</p>
-                    <p>Country: {address.Country}</p>
-                    <p>Postal Code: {address.PostalCode}</p>
+                    <p className='small-info'>{address.StreetName},</p>
+                    <p className='small-info'>{address.City},</p>
+                    <p className='small-info'>{address.Country},</p>
+                    <p className='small-info'>{address.PostalCode}</p>
                 </div>
             </div>
             <a className='edit-address'><i className="fa-solid fa-pen"></i></a>
             </div>
+            <div className='address-line'></div>
         </div>
     );
 };
--- WebApp/src/views/MyAddresses.tsx
+++ WebApp/src/views/MyAddresses.tsx
@@ -12,23 +12,23 @@ const MyAddresses = () => {
     const addresses: AddressData[] = [
         {
             Title: 'Home',
-            StreetName: '123 Main St',
-            City: 'Example City 1',
-            Country: 'Example Country 1',
+            StreetName: 'Main St',
+            City: 'City',
+            Country: 'Country',
             PostalCode: '12345',
         },
         {
             Title: 'Work',
-            StreetName: '456 Elm St',
-            City: 'Example City 2',
-            Country: 'Example Country 2',
+            StreetName: 'Elm St',
+            City: 'City',
+            Country: 'Country',
             PostalCode: '54321',
         },
         {
             Title: 'Vacation',
-            StreetName: '789 Oak St',
-            City: 'Example City 3',
-            Country: 'Example Country 3',
+            StreetName: 'Oak St',
+            City: 'City',
+            Country: 'Country',
             PostalCode: '67890',
         },
     ];
