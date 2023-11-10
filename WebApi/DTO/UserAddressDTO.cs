eadb7abf85dde48493b7ba4f3261f622c6e956d3
lirije <lirije11@hotmail.com>
2023-11-10 09:58:16 +0100

    small design fixes

--- WebApi/DTO/UserAddressDTO.cs
+++ WebApi/DTO/UserAddressDTO.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Streetname { get; set; }
+        public string StreetName { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public string PostalCode { get; set; }
