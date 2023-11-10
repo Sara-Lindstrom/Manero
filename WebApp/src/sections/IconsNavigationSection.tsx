97cbe0281fa2c761f2c3a0ea819db49952d1ccb7
lirije <lirije11@hotmail.com>
2023-11-10 12:23:28 +0100

    connection string removed

--- WebApp/src/sections/IconsNavigationSection.tsx
+++ WebApp/src/sections/IconsNavigationSection.tsx
@@ -5,11 +5,13 @@ interface IconsNavigationSectionProps {
 }
 
 const IconsNavigationSection: React.FC<IconsNavigationSectionProps> = ({ isAuthenticated }) => {
+    console.log('Is Authenticated:', isAuthenticated);
+
     return (
         <section className='iconsNav'>
             <div className='container'>
                 <div className='icons'>
-                    <div className='icon'><NavLink to="/" data-testid="homeLink" ><i className="fa-regular fa-house"></i></NavLink></div>
+                    <div className='icon'><NavLink to="/home" data-testid="homeLink" ><i className="fa-regular fa-house"></i></NavLink></div>
                     <div className='icon'><NavLink to="/search" data-testid="searchLink"><i className="fa-regular fa-magnifying-glass"></i></NavLink></div>
                     <div className='icon'><NavLink to="/cart" data-testid="cartLink"><i className="fa-regular fa-bag-shopping"></i></NavLink></div>
                     <div className='icon'><NavLink to="/wishlist" data-testid="wishlistLink"><i className="fa-regular fa-heart"></i></NavLink></div>
