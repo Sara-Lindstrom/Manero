e8d64a9b9e19a10e80cfa937c427c33ca23cae2c
lirije <lirije11@hotmail.com>
2023-11-10 12:35:50 +0100

    authentication after signing in to home page and viewprofile from home page should work

--- WebApp/src/sections/IconsNavigationSection.tsx
+++ WebApp/src/sections/IconsNavigationSection.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 interface IconsNavigationSectionProps {
     isAuthenticated: boolean;
@@ -6,6 +6,19 @@ interface IconsNavigationSectionProps {
 
 const IconsNavigationSection: React.FC<IconsNavigationSectionProps> = ({ isAuthenticated }) => {
     console.log('Is Authenticated:', isAuthenticated);
+
+    const navigate = useNavigate();
+
+    const handleProfileClick = () => {
+        // Check if the user is authenticated
+        if (isAuthenticated) {
+            // Navigate to the profile page
+            navigate('/viewProfile');
+        } else {
+            // Navigate to the sign-in page
+            navigate('/signin');
+        }
+    };
 
     return (
         <section className='iconsNav'>
@@ -15,7 +28,11 @@ const IconsNavigationSection: React.FC<IconsNavigationSectionProps> = ({ isAuthe
                     <div className='icon'><NavLink to="/search" data-testid="searchLink"><i className="fa-regular fa-magnifying-glass"></i></NavLink></div>
                     <div className='icon'><NavLink to="/cart" data-testid="cartLink"><i className="fa-regular fa-bag-shopping"></i></NavLink></div>
                     <div className='icon'><NavLink to="/wishlist" data-testid="wishlistLink"><i className="fa-regular fa-heart"></i></NavLink></div>
-                    {isAuthenticated && <div className='icon'><NavLink to="/viewProfile" data-testid="viewProfileLink"><i className="fa-regular fa-user"></i></NavLink></div>}
+                    <div className='icon'>
+                        <span onClick={handleProfileClick} data-testid="viewProfileLink">
+                            <i className="fa-regular fa-user"></i>
+                        </span>
+                    </div>
                 </div>
             </div>
         </section>
