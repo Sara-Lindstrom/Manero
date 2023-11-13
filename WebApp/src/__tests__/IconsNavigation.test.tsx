0495b6597835e70a88e89b4a2a74e07ee1501432
sara-lindstrom <sara.maria.lindstrom@hotmail.com>
2023-11-13 17:01:25 +0100

    small fix

--- WebApp/src/__tests__/IconsNavigation.test.tsx
+++ WebApp/src/__tests__/IconsNavigation.test.tsx
@@ -15,7 +15,7 @@ test('Should navigate to home', async () => {
     const homeNavLink = screen.getByTestId('homeLink');
 
     // Ensure that the "href" attribute of the link is '/'.
-    expect(homeNavLink.getAttribute('href')).toBe('/home');
+    expect(homeNavLink.getAttribute('href')).toBe('/');
 
     // Simulate a click event on the link.
     fireEvent.click(homeNavLink);
