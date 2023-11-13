03505b73a1f88fc08185c9aa95fc276b33ddd6c9
Ninjaraa <ninja.claesson@gmail.com>
2023-11-13 19:55:48 +0100

    Backend for productreviews and save review to db

--- WebApi/Context/ProductDbContext.cs
+++ WebApi/Context/ProductDbContext.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
 using WebApi.Models.Entities;
 
 namespace WebApi.Context
 {
     public class ProductDbContext : DbContext
     {
-        public ProductDbContext(DbContextOptions options) : base(options)
-        {
-        }
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
 
         DbSet<CategoryEntity> Categories { get; set; }
         DbSet<ColorEntity> Colors { get; set; }
@@ -16,10 +16,18 @@ namespace WebApi.Context
         DbSet<ProductColorEntity> ProductColors { get; set; }
         DbSet<ProductEntity> Products { get; set; }
         DbSet<ProductImageEntity> ProductImages { get; set; }
-        DbSet<ProductReviewEntity> ProductReviews { get; set; }
         DbSet<ProductSizeEntity> ProductSizes { get; set; }
-        DbSet<CategoryTagEntity> CategoryTags  { get; set; }
+        DbSet<CategoryTagEntity> CategoryTags { get; set; }
         DbSet<SizeEntity> Sizes { get; set; }
         DbSet<TagEntity> Tags { get; set; }
+
+        public DbSet<ProductReviewEntity> ProductReviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ProductReviewEntity>()
+                .Ignore(review => review.User);
+        }
     }
 }
